52f3b5e0b5cc6c6052992e5cac53c7090ba9fe47
NC5002 <ncwork.350@outlook.com>
2025-06-09 23:31:27 -0400

    Add sidebar for professor pages to enhance navigation

--- pages/profesor_configuracion.php
+++ pages/profesor_configuracion.php
@@ -21,6 +21,7 @@ $stmt->execute();
 $stmt->bind_result($nombre, $apellido);
 $stmt->fetch();
 $stmt->close();
+include __DIR__ . '/side_bar_profesor.php';
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -37,71 +38,7 @@ $stmt->close();
     <div class="container-fluid">
         <div class="row">
             <!-- Sidebar -->
-            <div class="col-md-3 col-lg-2 d-md-block sidebar collapse">
-                <div class="position-sticky pt-3">
-                    <div class="text-center mb-4">
-                        <h4 class="text-white">Sistema Académico</h4>
-                        <p class="text-white-50">Panel de Profesor</p>
-                    </div>
-                    <ul class="nav flex-column">
-                        <li class="nav-item">
-                            <a class="nav-link" href="profesor_dashboard.php">
-                                <i class="bi bi-house-door me-2"></i>
-                                Dashboard
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="profesor_cursos.php">
-                                <i class="bi bi-book me-2"></i>
-                                Mis Cursos
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="profesor_estudiantes.php">
-                                <i class="bi bi-people me-2"></i>
-                                Estudiantes
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="profesor_calificaciones.php">
-                                <i class="bi bi-award me-2"></i>
-                                Calificaciones
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="profesor_asistencia.php">
-                                <i class="bi bi-calendar-check me-2"></i>
-                                Asistencia
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="profesor_tareas.php">
-                                <i class="bi bi-clipboard-check me-2"></i>
-                                Tareas
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="profesor_materiales.php">
-                                <i class="bi bi-file-earmark-text me-2"></i>
-                                Materiales
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="profesor_perfil.php">
-                                <i class="bi bi-person-circle me-2"></i>
-                                Mi Perfil
-                            </a>
-                        </li>
-                        <li class="nav-item mt-5">
-                            <a class="nav-link" href="../index.php">
-                                <i class="bi bi-box-arrow-left me-2"></i>
-                                Cerrar Sesión
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-
+           
             <!-- Main content -->
             <div class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
                 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
